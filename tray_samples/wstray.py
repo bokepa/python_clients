afb802e9e0df4144a26014b5b425b8dca6545e84
Daniel Hernandez <6077585+bokepa@users.noreply.github.com>
2019-09-03 12:25:20 +0200

    adding samples

---
+++ tray_samples/wstray.py
@@ -0,0 +1,37 @@
+import wx.adv
+import wx
+TRAY_TOOLTIP = 'Name' 
+TRAY_ICON = 'icon.png' 
+
+
+def create_menu_item(menu, label, func):
+    item = wx.MenuItem(menu, -1, label)
+    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
+    menu.Append(item)
+    return item
+
+
+class TaskBarIcon(wx.adv.TaskBarIcon):
+    def __init__(self, frame):
+        self.frame = frame
+        super(TaskBarIcon, self).__init__()
+        self.set_icon(TRAY_ICON)
+        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+
+    def CreatePopupMenu(self):
+        menu = wx.Menu()
+        create_menu_item(menu, 'Site', self.on_hello)
+        menu.AppendSeparator()
+        create_menu_item(menu, 'Exit', self.on_exit)
+        return menu
+
+    def set_icon(self, path):
+        icon = wx.Icon(path)
+        self.SetIcon(icon, TRAY_TOOLTIP)
+
+    def on_left_down(self, event):      
+        print ('Tray icon was left-clicked.')
+
+app = wx.PySimpleApp()
+TaskBarIcon()
+app.MainLoop()
